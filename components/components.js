7f9874e9883ce776681397c15039525cd490cc86
Szymon Korytnicki <szymon.korytnicki@gmail.com>
2022-11-16 20:28:58 +0100

    Lint fix

--- components/components.js
+++ components/components.js
@@ -2,7 +2,7 @@ import * as style from "./components.css";
 
 function styled(Component) {
   return function (className) {
-    return function (props) {
+    return function StyledComponent(props) {
       // TODO use classNames to merge the className prop with the className we want to apply
       return <Component className={className} {...props} />;
     };
